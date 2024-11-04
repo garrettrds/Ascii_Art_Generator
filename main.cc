8e3a14e0a4d8282bb163266304c9081603bc3bb0
Garrett <99564508+Ilphu@users.noreply.github.com>
2024-11-04 18:56:34 -0500

    OOP rewrite

--- main.cc
+++ main.cc
@@ -16,171 +16,252 @@ extern "C" {
 
 using namespace std;
 
+#ifndef IMAGE_H
+#define IMAGE_H
+
+class Image {
+public:
+// Public methods
+    Image();
+    Image(string filename);
+    ~Image();
+
+    void to_ascii(const int& scalar);
+    bool load();
+    int get_width() const;
+    int get_height() const;
+private:
+// Private methods
+    void scaled_greyscale_image();
+    int convolve(const int& x_pos, const int& y_pos) const;
+    int convolve(const vector<vector<int> >& matrix, const int& x_pos, 
+                 const int& y_pos, const vector<int>& kernel);
+    
+    double convolve(const int& x_pos, const int& y_pos, 
+                    const vector<vector<int> >& kernel);
+
+    bool sobel(const int& x_pos, const int& y_pos, double& theta);
+    void gaussian_blur(vector<vector<int> >& blurred_image, 
+                       const vector<int>& kernel);
+    void dog(); // woof
+// Attributes
+    vector<unsigned char> _image;
+    vector<vector<int> > _greyscale_image;
+    vector<vector<int> > _dog;
+    int _width;
+    int _height;
+    int _scaled_width;
+    int _scaled_height;
+    int _scalar;
+    string _filename;
+    const string _ascii_luminance = " .:cCoPO0@";
+};
+
+#endif
+
 /**
- * @brief Prints a matrix
+ * @brief Construct a new Image object
  * 
- * @param matrix - matrix to print
- * @param width - width of matrix
- * @param height - height of matrix
  */
-void print_matrix(const vector<vector<int> > & matrix, const int & width, const int & height) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            int val = matrix[i][j];
-            if (val < 10) {
-                cout << "  " << val << " ";
-            } else if (val < 100) {
-                cout << " " << val << " ";
-            } else {
-                cout << val << " ";
-            }
-        }
-        cout << endl;
-    }
+Image::Image() {}
+
+/**
+ * @brief Construct a new Image object
+ * 
+ * @param _filename - name of image file
+ */
+Image::Image(string filename) {
+    _filename = filename;
 }
 
 /**
- * @brief Loads the image into a vecotr storing rgb data
- * 
- * @param image - the array to stor info in
- * @param filename - name of image file 
- * @param x
- * @param y 
- * @return true - if the image was successfully loaded
- * @return false  - if the image wasn't successfully loaded
+ * @brief Destroy the Image object
  */
-bool load_image(vector<unsigned char>& image, const string& filename, int& x, int&y) {
+Image::~Image() {}
+
+/**
+ * @brief Converts _image to ascii and exports it to output.txt
+ * 
+ * @param scalar - how much to down scale the image
+ */
+void Image::to_ascii(const int& scalar) {
+    _scalar = scalar;
+
+    _scaled_width = _width / _scalar;
+    _scaled_height = _height / _scalar;
+
+    string output_filename = "output.txt";
+    ofstream out(output_filename);
+
+    int avg_lumin;
+    int ascii_idx;
+    double theta;
+
+    scaled_greyscale_image();
+
+    dog(); // woof
+    
+    for (int i = 0; i < _scaled_height; i++) {
+        for (int j = 0; j < _scaled_width; j++) {
+            if ((!((i == 0) || (j == 0) || (i == (_scaled_height - 1)) || 
+                   (j == (_scaled_width - 1)))) && (sobel(j, i, theta)) &&
+                   (_greyscale_image[i][j] < 96)) {
+                
+                char back_slash = 92;
+                
+                if ((theta < 0.1) || (theta > 0.9)) {
+                    out << "--";
+                } else if (theta < 0.4) {
+                    out << back_slash << back_slash;
+                } else if (theta < 0.6) {
+                    out << "||";
+                } else {
+                    out << "//";
+                }
+            } else {
+                avg_lumin = _greyscale_image[i][j] * 100;
+                ascii_idx = (avg_lumin / (25500 / 
+                (_ascii_luminance.length() - 1)));
+                
+                out << _ascii_luminance[ascii_idx] << _ascii_luminance[ascii_idx];
+            }
+        }
+        out << endl;
+    }
+    out.close();
+}
+
+/**
+ * @brief Gets _width attribute
+ * 
+ * @return int 
+ */
+int Image::get_width() const {
+    return _width;
+}
+
+/**
+ * @brief Gets height attribute
+ * 
+ * @return int 
+ */
+int Image::get_height() const {
+    return _height;
+}
+
+/**
+ * @brief Loads the needed info into the _image attribute
+ * 
+ * @return true 
+ * @return false 
+ */
+bool Image::load() {
     int n;
-    unsigned char* data = stbi_load(filename.c_str(), &x, &y, &n, 4);
+    unsigned char* data = stbi_load(_filename.c_str(),&_width,&_height,&n, 4);
     if (data != nullptr) {
-        image = vector<unsigned char>(data, data + x * y * 4);
+        _image = vector<unsigned char>(data, data + _width * _height * 4);
     }
     stbi_image_free(data);
     return (data != nullptr);
 }
 
 /**
- * @brief Computes a gaussain blure and puts it in the blurred_matrix
+ * @brief Finds the average luminance of an area of the iamge based on the scalar
  * 
- * @param blurred_matrix - the matrix where the blur is stored
- * @param lumin_matrix - greyscale matrix
- * @param width - width of grayscale matrix
- * @param height - height of greyscale matrix
- * @param kernel - blur kernel
- * @param kernel_sum - sum of blur kernel
- */
-void gaussian_blur(vector<vector<int> > & blurred_matrix, 
-                   const vector<vector<int> > & lumin_matrix, 
-                   const int & width, const int & height,
-                   const vector<int> & kernel, const int & kernel_sum) {
-
-    vector<int> blurred_matrix_row;
-    int kernel_size = (log2(kernel_sum) / 2) + 1;
-    int blur = 0;
-
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (!(((i - ((kernel_size / 2) - 1)) <= 0) || 
-                  ((j - ((kernel_size / 2) - 1)) <= 0) || 
-                  ((i + ((kernel_size / 2) - 1)) >= (height - 1)) || 
-                  ((j + ((kernel_size / 2) - 1)) >= (width - 1)))) {
-
-                for (int k = 0; k < kernel_size; k++) {
-                    for (int l = 0; l < kernel_size; l++) {
-                        blur += lumin_matrix[i + k - (kernel_size / 2)][j + l - (kernel_size / 2)] * (kernel[k] * kernel[l]);
-                    }
-                }
-                blur = blur / kernel_sum;
-                blurred_matrix_row.push_back(blur);
-            } else {
-                blurred_matrix_row.push_back(0);
-            }
-        }
-        blurred_matrix.push_back(blurred_matrix_row);
-        blurred_matrix_row.clear();
-    }
-}
-
-/**
- * @brief Calculates the average luminance of a sub array based on the 
- *        downscaling factor
- * 
- * @param image - image vector with stored rgb data
- * @param width - number of pixels wide an image is
- * @param scalar - the downscaling factor
- * @param x_pos - x position of top left corner of the sub-pixel
- * @param y_pos - y position of top left corner of the sub-pixel
+ * @param x_pos - x position of the pixel 
+ * @param y_pos - y position of the pixel
  * @return int - average luminance
  */
-int avg_luminance(const vector<unsigned char> & image, 
-                  const int & width, const int & scalar, 
-                  const int & x_pos, const int & y_pos) {
-
+int Image::convolve(const int& x_pos, const int& y_pos) const {
     const size_t RGBA = 4;
     int r, g, b;
     int avg_lumin = 0;
 
-    for (int i = (y_pos * scalar); i < (scalar * (y_pos + 1)); i++) {
-        for (int j = (x_pos * scalar); j < (scalar * (x_pos + 1)); j++) {
-            size_t index = RGBA * (i * width + j);
-            r = static_cast<int>(image[index + 0]);
-            g = static_cast<int>(image[index + 1]);
-            b = static_cast<int>(image[index + 2]);
+    for (int i = (y_pos * _scalar); i < (_scalar * (y_pos + 1)); i++) {
+        for (int j = (x_pos * _scalar); j < (_scalar * (x_pos + 1)); j++) {
+            size_t index = RGBA * (i * _width + j);
+            r = static_cast<int>(_image[index + 0]);
+            g = static_cast<int>(_image[index + 1]);
+            b = static_cast<int>(_image[index + 2]);
 
             avg_lumin += (r + g + b) / 3;
         }
     }
-    avg_lumin = avg_lumin / (scalar * scalar);
+    avg_lumin = avg_lumin / (_scalar * _scalar);
     return avg_lumin;
 }
 
 /**
- * @brief Converts the original image to the scaled down grayscaled image
- * 
- * @param lumin_matrix - finished scaled grayscale matrix
- * @param image - original image data
- * @param width - width of original image
- * @param height - height of original image
- * @param end_width - sets this to the final width of the image
- * @param end_height - sets this to the final height of the image
- * @param scalar - the downscaling factor of the image
+ * @brief Scales the image and greyscales it
  */
-void scaled_lumin_matrix(vector<vector<int> > & lumin_matrix, 
-                         const vector<unsigned char> & image, 
-                         const int & width, const int & height,
-                         int & end_width, int & end_height,  
-                         const int & scalar) {
+void Image::scaled_greyscale_image() {
+    vector<int> greyscale_row;
 
-    end_width = width / scalar;
-    end_height = height / scalar;
-    int avg_lumin = 0;
-    vector<int> lumin_matrix_row;
-
-    for (int i = 0; i < end_height; i++) {
-        for (int j = 0; j < end_width; j++) {
-            avg_lumin = avg_luminance(image, width, scalar, j, i);
-            lumin_matrix_row.push_back(avg_lumin);
+    for (int i = 0; i < _scaled_height; i++) {
+        for (int j = 0; j < _scaled_width; j++) {  
+            greyscale_row.push_back(convolve(j, i));
         }
-        lumin_matrix.push_back(lumin_matrix_row);
-        lumin_matrix_row.clear();
+        _greyscale_image.push_back(greyscale_row);
+        greyscale_row.clear();
     }
 }
 
 /**
- * @brief Detects if if the pixel at x_pos, y_pos is a part of an edge using 
- *        the sobel edge detection algorithm
+ * @brief Convolves the kernel with the _greyscale_image at pixel x_pos y_pos
  * 
- * @param lumin_matrix - scaled brightness image
- * @param x_pos - x position of the pixel we want to check
- * @param y_pos - y position of the pixel we want to check
- * @param theta - angle of the edge if detected [0, 1]
- * @return true - edge was detected
- * @return false - edge wasn't detected
+ * @param x_pos - x position of the pixel being convolved
+ * @param y_pos - y position of the pixel being convolved
+ * @param kernel - kernel to do the convolution
+ * @return double - sum total after convolution
  */
-bool edge_dector(const vector<vector<int> > & lumin_matrix, 
-                 const int & x_pos, const int & y_pos,
-                 double & theta) {
+double Image::convolve(const int& x_pos, const int& y_pos, 
+                       const vector<vector<int> >& kernel) {
+    int kernel_size = kernel.size();
+    double total = 0.0;
+    for (int i = 0; i < kernel_size; i++) {
+        for (int j = 0; j < kernel_size; j++) {
+            total += _greyscale_image[y_pos + i - (kernel_size / 2)][x_pos + j - 
+            (kernel_size / 2)] * kernel[i][j];
+        }
+    }
+    return (total);
+}
 
+/**
+ * @brief Convolves matrix with kernel at x_pos y_pos of matrix
+ * 
+ * @param matrix - 2d array
+ * @param x_pos - x position to convolve
+ * @param y_pos - y position to convolve
+ * @param kernel - kernel to convolve with
+ * @return int - adjusted total
+ */
+int Image::convolve(const vector<vector<int> >& matrix, const int& x_pos, 
+                    const int& y_pos, const vector<int>& kernel) {
+
+    int total = 0;
+    int kernel_size = kernel.size();
+    int kernel_sum = pow(2, 2 * (kernel_size + 1));
+    for (int i = 0; i < kernel_size; i++) {
+        for (int j = 0; j < kernel_size; j++) {
+            total += matrix[y_pos + i - (kernel_size / 2)][x_pos + j - 
+            (kernel_size / 2)] * (kernel[i] * kernel[j]);
+        }
+    }
+    total = total / kernel_sum;
+    return (total);
+}
+
+/**
+ * @brief Detects edges at x_pos y_pos and gives the angle of the edge
+ * 
+ * @param x_pos - x position of pixel to check
+ * @param y_pos - y position of pixel to check
+ * @param theta - angle of edge [0, 1]
+ * @return true - if there is an edge
+ * @return false - if there is not an edge
+ */
+bool Image::sobel(const int& x_pos, const int& y_pos, double& theta) {
     vector<vector<int> > x_kernel;
     vector<int> x_kernel_row;
     
@@ -219,15 +300,9 @@ bool edge_dector(const vector<vector<int> > & lumin_matrix,
     y_kernel.push_back(y_kernel_row);
     y_kernel_row.clear();
 
-    double Gx = 0.0001;
-    double Gy = 0.0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            Gx += lumin_matrix[i + y_pos - 1][j + x_pos - 1] * x_kernel[i][j];
-            Gy += lumin_matrix[i + y_pos - 1][j + x_pos - 1] * y_kernel[i][j];
-        }
-    }
-    theta = 0.0;
+    double Gx = convolve(x_pos, y_pos, x_kernel) + 0.0001;
+    double Gy = convolve(x_pos, y_pos, y_kernel);
+
     if (sqrt((Gx * Gx) + (Gy * Gy)) > 400) {
         theta = (atan(Gy / Gx) / M_PI) + 0.5;
         return true;
@@ -236,28 +311,40 @@ bool edge_dector(const vector<vector<int> > & lumin_matrix,
 }
 
 /**
- * @brief Converts the average luminance of each pixel, or sub-pixel, to an 
- *        ascii value 
+ * @brief Blurs the image and puts it in blurred_image
  * 
- * @param image - image data vector
- * @param width - original width of image
- * @param height - original height of image
- * @param scalar - downscaling factor
- * @param ascii_luminance - encodes the brightness of every ascii character
+ * @param blurred_image - storage for blurred image
+ * @param kernel - how much u wanna blur?
  */
-void image_to_ascii(const vector<unsigned char> & image, 
-                    const int & width, const int & height, 
-                    const int & scalar, const string & ascii_luminance) {
+void Image::gaussian_blur(vector<vector<int> >& blurred_image, 
+                          const vector<int>& kernel) {
+    vector<int> blurred_image_row;
+    int border = (kernel.size() / 2) - 1;
     
-    string output_filename = "output.txt";
-    ofstream out(output_filename);
-    
-    vector<vector<int> > lumin_matrix;
-    int end_width;
-    int end_height;
+    int blur;
 
-    scaled_lumin_matrix(lumin_matrix, image, width, height, end_width, end_height, scalar);
+    for (int i = 0; i < _scaled_height; i++) {
+        for (int j = 0; j < _scaled_width; j++) {
+            if (!(((i - border) <= 0) || ((j - border) <= 0) || 
+                  ((i + border) >= (_scaled_height - 1)) || 
+                  ((j + border) >= (_scaled_width - 1)))) {
+                
+                blur = convolve(_greyscale_image, j, i, kernel);
+                blurred_image_row.push_back(blur);
+            } else {
+                blurred_image_row.push_back(0);
+            }
+        }
+        blurred_image.push_back(blurred_image_row);
+        blurred_image_row.clear();
+    }
+}
 
+/**
+ * @brief Summons the Devourer of-- sorry, preforms a simple difference of 
+ *        gaussians with a threshold of 9 and kernel sizes of 3 and 7
+ */
+void Image::dog() {
     vector<int> kernel_1;
     kernel_1.push_back(1);
     kernel_1.push_back(2);
@@ -272,55 +359,24 @@ void image_to_ascii(const vector<unsigned char> & image,
     kernel_2.push_back(6);
     kernel_2.push_back(1);
 
-    vector<vector<int> > gause_1;
-    vector<vector<int> > gause_2;
-    gaussian_blur(gause_1, lumin_matrix, end_width, end_height, kernel_1, 16);
-    gaussian_blur(gause_2, lumin_matrix, end_width, end_height, kernel_2, 4096);
-    vector<vector<int> > dog_matrix;
-    vector<int> dog_row;
+    vector<vector<int> > blur_1;
+    vector<vector<int> > blur_2;
 
-    for (int i = 0; i < end_height; i++) {
-        for (int j = 0; j < end_width; j++) {
-            if ((gause_1[i][j] - gause_2[i][j]) > 9) {
+    gaussian_blur(blur_1, kernel_1);
+    gaussian_blur(blur_2, kernel_2);
+
+    vector<int> dog_row;
+    for (int i = 0; i < _scaled_height; i++) {
+        for (int j = 0; j < _scaled_width; j++) {
+            if ((blur_1[i][j] - blur_2[i][j]) > 9) {
                 dog_row.push_back(255);
             } else {
                 dog_row.push_back(0);
             }
         }
-        dog_matrix.push_back(dog_row);
+        _dog.push_back(dog_row);
         dog_row.clear();
     }
-
-    int avg_lumin;
-    int ascii_idx;
-    double theta;
-
-    for (int i = 0; i < end_height; i++) {
-        for (int j = 0; j < end_width; j++) {
-            if ((!((i == 0) || (j == 0) || (i == (end_height - 1)) || (j == (end_width - 1)))) && 
-                (edge_dector(dog_matrix, j, i, theta)) && (lumin_matrix[i][j] < 96)) {
-
-                char back_slash = 92;
-
-                if ((theta < 0.1) || (theta > 0.9)) {
-                    out << "--";
-                } else if (theta < 0.4) {
-                    out << back_slash << back_slash;
-                } else if (theta < 0.6) {
-                    out << "||";
-                } else {
-                    out << "//";
-                }
-            } else {
-                avg_lumin = lumin_matrix[i][j] * 100;
-                ascii_idx = (avg_lumin / (25500 / (ascii_luminance.length() - 1)));
-                
-                out << ascii_luminance[ascii_idx] << ascii_luminance[ascii_idx];
-            }
-        }
-        out << endl;
-    }
-    out.close();
 }
 
 /**
@@ -331,24 +387,21 @@ void image_to_ascii(const vector<unsigned char> & image,
 int main() {
     string img_filename;
     int scalar;
-    cout << "File name:" << endl;
+    cout << "File name:\n";
     cin >> img_filename;
     
-    int width, height;
-    vector<unsigned char> image;
-    bool success = load_image(image, img_filename, width, height);
+    Image img(img_filename);
+    bool success = img.load();
     if (!success) {
         cout << "Error loading image\n";
         return 1;
     }
 
-    cout << "Input image dimensions:" << endl << width << " x " << height << endl;
-    cout << "Image downscaling factor:" << endl;
+    cout << "Input image dimensions:\n" << img.get_width() <<
+    " x " << img.get_height() << endl;
     cin >> scalar;
-    
-    string ascii_luminance = " .:cCoPO0@";
 
-    image_to_ascii(image, width, height, scalar, ascii_luminance);
+    img.to_ascii(scalar);
 
     return 0;
 }
